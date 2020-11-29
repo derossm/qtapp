de8cfd5833dd5e05668912a313e9d983d663aab3
Mason DeRoss <derossm@gmail.com>
2020-11-29 10:47:09 -0800

    everything ready to start writing tests

--- utest/test_main.cpp
+++ utest/test_main.cpp
@@ -2,13 +2,31 @@
 
 \* ************************************************************************************************************************************************ */
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
 #include "..\QtApp\QtApp.h"
 
+#include <charconv>
+#include <new>
+
+static int argc_;
+static char** argv_;
+
+int main(int argc, char* argv[])
+{
+	argc_ = argc;
+	argv_ = argv;
+
+	int result = Catch::Session().run(argc, argv);
+}
+
 TEST_CASE("Test", "[QtApp]")
 {
-	//QtApp obj;
-	//REQUIRE(obj);
+	auto argc = argc_;
+	auto argv = new char*(*argv_);
+	QApplication app(argc, argv);
+	QtApp obj;
+	REQUIRE(obj.isEnabled());
+	delete argv;
 }
