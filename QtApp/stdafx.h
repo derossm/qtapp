af4c6e6816a25665af2cbb8a32e09660a8bc1452
Mason DeRoss <derossm@gmail.com>
2020-12-09 01:15:53 -0800

    commenting out unused headers

--- QtApp/stdafx.h
+++ QtApp/stdafx.h
@@ -4,8 +4,8 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <Windows.h>
+//#define NOMINMAX
+//#include <Windows.h>
 
 // Concepts library
 #include <concepts>
@@ -19,41 +19,41 @@
 //#include <csetjmp>
 //#include <cstdarg>
 #include <typeinfo>
-#include <typeindex>
+//#include <typeindex>
 #include <type_traits>
-#include <bitset>
+//#include <bitset>
 #include <functional>
 #include <utility>
 //#include <ctime>
-#include <chrono>
+//#include <chrono>
 //#include <cstddef>
-#include <initializer_list>
-#include <tuple>
-#include <any>
-#include <optional>
-#include <variant>
-#include <compare>
-#include <version>
+//#include <initializer_list>
+//#include <tuple>
+//#include <any>
+//#include <optional>
+//#include <variant>
+//#include <compare>
+//#include <version>
 //#include <source_location>
 
 // Dynamic memory management
-#include <new>
+//#include <new>
 #include <memory>
-#include <scoped_allocator>
-#include <memory_resource>
+//#include <scoped_allocator>
+//#include <memory_resource>
 
 // Numeric limits
 //#include <climits>
 //#include <cfloat>
 //#include <cstdint>
 //#include <cinttypes>
-#include <limits>
+//#include <limits>
 
 // Error handling
-#include <exception>
-#include <stdexcept>
+//#include <exception>
+//#include <stdexcept>
 //#include <cassert>
-#include <system_error>
+//#include <system_error>
 //#include <cerrno>
 
 // Strings library
@@ -65,79 +65,79 @@
 #include <string>
 #include <string_view>
 #include <charconv>
-//#include <format>
+//#include <format> // C++20 - not available yet
 
 // Containers library
 #include <array>
 #include <vector>
-#include <deque>
+//#include <deque>
 #include <list>
-#include <forward_list>
-#include <set>
+//#include <forward_list>
+//#include <set>
 #include <map>
-#include <unordered_set>
-#include <unordered_map>
-#include <stack>
-#include <queue>
-#include <span>
+//#include <unordered_set>
+//#include <unordered_map>
+//#include <stack>
+//#include <queue>
+//#include <span>
 
 // Iterators library
-#include <iterator>
+//#include <iterator>
 
 // Ranges library
 #include <ranges>
 
 // Algorithms library
 #include <algorithm>
-#include <execution>
+//#include <execution>
 
 // Numerics library
 //#include <cmath>
-#include <complex>
-#include <valarray>
-#include <random>
-#include <numeric>
-#include <ratio>
+//#include <complex>
+//#include <valarray>
+//#include <random>
+//#include <numeric>
+//#include <ratio>
 //#include <cfenv>
-#include <bit>
-#include <numbers>
+//#include <bit>
+//#include <numbers>
 
 // Localization library
-#include <locale>
+//#include <locale>
 //#include <clocale>
 
 // Input/output library
-#include <iosfwd>
-#include <ios>
-#include <istream>
-#include <ostream>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-//#include <syncstream>
-#include <iomanip>
-#include <streambuf>
+//#include <iosfwd>
+//#include <ios>
+//#include <istream>
+//#include <ostream>
+//#include <iostream>
+//#include <fstream>
+//#include <sstream>
+//#include <syncstream> // C++20 - not available yet
+//#include <iomanip>
+//#include <streambuf>
 //#include <cstdio>
 
 // Filesystem library
-#include <filesystem>
+//#include <filesystem>
 
 // Regular Expressions library
 #include <regex>
 
 // Atomic Operations library
-#include <atomic>
+//#include <atomic>
 
 // Thread support library
-#include <thread>
-#include <stop_token>
-#include <mutex>
-#include <shared_mutex>
-#include <future>
-#include <condition_variable>
-#include <semaphore>
-#include <latch>
-#include <barrier>
+//#include <thread>
+//#include <stop_token>
+//#include <mutex>
+//#include <shared_mutex>
+//#include <future>
+//#include <condition_variable>
+//#include <semaphore>
+//#include <latch>
+//#include <barrier>
 
 #include <gsl/gsl>
 
